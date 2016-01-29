a4b31e635627c5030955514d0a01d4b8cb59cd3b
Vincent Le <vinnyoodles@gmail.com>
2016-01-28 20:01:23 -0500

    made app prototype

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     
+<<<<<<< Updated upstream
+=======
+    @IBOutlet weak var tableView: UITableView!
+>>>>>>> Stashed changes
     var feed: [NSDictionary]?
 
     override func viewDidLoad() {
@@ -31,14 +35,57 @@ class PhotosViewController: UIViewController {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
+<<<<<<< Updated upstream
                             self.feed = responseDictionary
+=======
+                            self.feed = responseDictionary["data"] as? [NSDictionary]
+>>>>>>> Stashed changes
                     }
                 }
         });
+        print(feed)
         task.resume()
         
     }
+<<<<<<< Updated upstream
 
+=======
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 20
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        
+        
+        
+        /*let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let posterUrl = NSURL(string: posterBaseUrl + posterPath)
+            cell.posterView.setImageWithURL(posterUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            cell.posterView.image = nil
+        }
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        print("row \(indexPath.row)")*/
+        
+        
+        return cell
+    }
+>>>>>>> Stashed changes
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Instagram/feedCell.swift
@@ -0,0 +1,25 @@
+//
+//  feedCell.swift
+//  Instagram
+//
+//  Created by Vincent Le on 1/28/16.
+//  Copyright © 2016 Parker Robinson. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
